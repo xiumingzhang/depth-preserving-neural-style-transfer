946a63149ad1014952d4d2ad118e818bf7657755
Yu Xia <aluex.cpp@gmail.com>
2016-12-03 17:03:59 -0500

    update

--- fast_neural_style/DepthLoss.lua
+++ fast_neural_style/DepthLoss.lua
@@ -4,3 +4,20 @@ require 'nn'
 require 'fast_neural_style.GramMatrix'
 -- Depth Loss NN goes here
 
+function DepthLoss:__init(strength, loss_type, agg_type)
+
+end
+
+function DepthLoss:updateOutput(input)
+
+end
+
+function DepthLoss:updateGradInput(input, gradOutput)
+end
+
+function DepthLoss:setMode(mode)
+  if mode ~= 'capture' and mode ~= 'loss' and mode ~= 'none' then
+    error(string.format('Invalid mode "%s"', mode))
+  end
+  self.mode = mode
+end
